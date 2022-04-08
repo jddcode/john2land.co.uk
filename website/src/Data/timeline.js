aae3e48b5628d50068d9d522ea375eb4a21f67bd
John Driscoll <git@jdd.email>
2022-04-08 09:37:06 +0100

    Added env and timeline

---
+++ website/src/Data/timeline.js
@@ -0,0 +1,408 @@
+const timeline = `
+Checkpoints:
+  - Destination: John O Groats
+    Date: 2003-09-18 11:12:07
+    Road: A99
+    Image: 0
+  - Destination: John O Groats
+    Date: 2003-09-19 06:43:44
+    Road: A99
+    Image: 1
+  - Destination: Wick
+    Date: 2003-09-19 07:46:58
+    Road: A99
+    Image: 2
+  - Destination: Dumbeath
+    Date: 2003-09-19 09:25:36
+    Road: A99
+    Image: 3
+  - Destination: Helmsdale
+    Date: 2003-09-19 11:03:36
+    Road: A9
+    Image: 4
+  - Destination: Bora
+    Date: 2003-09-19 12:02:13
+    Road: A9
+    Image: 5
+  - Destination: Golspie
+    Date: 2003-09-19 12:33:09
+    Road: A9
+    Image: 6
+  - Destination: Dornoch Firth
+    Date: 2003-09-19 14:42:46
+    Road: A9
+    Image: 7
+  - Destination: Tain
+    Date: 2003-09-19 14:54:15
+    Road: A9
+    Image: 8
+  - Destination: Alness
+    Date: 2003-09-19 17:12:24
+    Road: A9
+  - Destination: Cromarty Firth
+    Date: 2003-09-19 17:15:00
+    Road: A9
+    Image: 10
+  - Destination: Tore
+    Date: 2003-09-19 17:15:40
+    Road: A9
+  - Destination: Moray Firth
+    Date: 2003-09-19 17:20:37
+    Road: A9
+  - Destination: Inverness
+    Date: 2003-09-19 17:21:55
+    Road: A82
+  - Destination: Drumnadrochit
+    Date: 2003-09-19 18:48:31
+    Road: A82
+  - Destination: Drumnadrochit
+    Date: 2003-09-19 18:48:49
+    Road: A82
+    Image: 15
+  - Destination: Invermoriston
+    Date: 2003-09-20 08:39:42
+    Road: A82
+    Image: 16
+  - Destination: Fort Augustus
+    Date: 2003-09-20 09:48:54
+    Road: A82
+    Image: 17
+  - Destination: Invergarry
+    Date: 2003-09-20 11:11:57
+    Road: A82
+    Image: 18
+  - Destination: Lach Lochy
+    Date: 2003-09-20 11:29:16
+    Road: A82
+    Image: 19
+  - Destination: Spear Bridge
+    Date: 2003-09-20 12:06:37
+    Road: A82
+    Image: 20
+  - Destination: Fort William
+    Date: 2003-09-20 13:36:02
+    Road: A82
+    Image: 21
+  - Destination: Orich
+    Date: 2003-09-20 13:54:59
+    Road: A82
+    Image: 22
+  - Destination: North Ballachuish
+    Date: 2003-09-20 14:52:41
+    Road: A82
+    Image: 23
+  - Destination: Glen Coe
+    Date: 2003-09-20 14:52:46
+    Road: A82
+    Image: 24
+  - Destination: Glen Coe
+    Date: 2003-09-21 07:20:32
+    Road: A82
+    Image: 25
+  - Destination: Bridge Of Orchie
+    Date: 2003-09-21 09:06:06
+    Road: A82
+    Image: 26
+  - Destination: Tyndrum
+    Date: 2003-09-21 09:42:22
+    Road: A82
+    Image: 27
+  - Destination: Crainlarich
+    Date: 2003-09-21 10:03:37
+    Road: A82
+    Image: 28
+  - Destination: Loch Lomond
+    Date: 2003-09-21 12:26:12
+    Road: A82
+    Image: 29
+  - Destination: Tarbet
+    Date: 2003-09-21 13:03:39
+    Road: A82
+    Image: 30
+  - Destination: Luss
+    Date: 2003-09-21 13:22:10
+    Road: A82
+    Image: 31
+  - Destination: Alexandria
+    Date: 2003-09-21 14:03:52
+    Road: A82
+    Image: 32
+  - Destination: Dumbarton
+    Date: 2003-09-21 14:27:08
+    Road: A82
+    Image: 33
+  - Destination: Glasgow (Govan)
+    Date: 2003-09-21 15:26:34
+    Road: Local
+    Image: 34
+  - Destination: Glasgow (Govan)
+    Date: 2003-09-22 08:07:10
+    Road: Local
+    Image: 35
+  - Destination: Hamilton
+    Date: 2003-09-22 08:42:07
+    Road: Local
+    Image: 36
+  - Destination: Larkhall
+    Date: 2003-09-22 09:41:59
+    Road: B7078
+    Image: 37
+  - Destination: Abington
+    Date: 2003-09-22 11:41:30
+    Road: A702
+    Image: 38
+  - Destination: Moffat
+    Date: 2003-09-22 13:05:53
+    Road: A702
+    Image: 39
+  - Destination: Lockerbie
+    Date: 2003-09-22 13:48:46
+    Road: B7076
+    Image: 40
+  - Destination: Gretna Green
+    Date: 2003-09-22 15:25:05
+    Road: A74
+    Image: 41
+  - Destination: Carlisle
+    Date: 2003-09-22 21:10:24
+    Road: A7
+    Image: 42
+  - Destination: Carlisle
+    Date: 2003-09-23 07:30:40
+    Road: A7
+    Image: 43
+  - Destination: Penrith
+    Date: 2003-09-23 08:59:16
+    Road: A6
+    Image: 44
+  - Destination: Shap Fells
+    Date: 2003-09-23 12:23:48
+    Road: A6
+    Image: 45
+  - Destination: Kendal
+    Date: 2003-09-23 13:10:08
+    Road: A6
+    Image: 46
+  - Destination: Carnforth
+    Date: 2003-09-23 14:38:20
+    Road: A6
+    Image: 47
+  - Destination: Lancaster
+    Date: 2003-09-23 15:04:02
+    Road: A6
+    Image: 48
+  - Destination: Garstang
+    Date: 2003-09-23 15:53:56
+    Road: A6
+    Image: 49
+  - Destination: Preston
+    Date: 2003-09-23 16:44:50
+    Road: A6
+    Image: 50
+  - Destination: Preston
+    Date: 2003-09-24 07:48:52
+    Road: A6
+    Image: 51
+  - Destination: Bamber Bridge
+    Date: 2003-09-24 08:03:11
+    Road: A6
+    Image: 52
+  - Destination: Leyland
+    Date: 2003-09-24 08:10:14
+    Road: A49
+  - Destination: Chorley
+    Date: 2003-09-24 08:18:51
+    Road: A49
+  - Destination: Wigan
+    Date: 2003-09-24 09:26:53
+    Road: A49
+  - Destination: Ashton In Makefield
+    Date: 2003-09-24 10:19:58
+    Road: A49
+    Image: 56
+  - Destination: Warrington
+    Date: 2003-09-24 10:54:55
+    Road: A49
+    Image: 57
+  - Destination: Knutsford
+    Date: 2003-09-24 11:48:28
+    Road: A50
+    Image: 58
+  - Destination: Holmes Chapel
+    Date: 2003-09-24 12:20:51
+    Road: A50
+    Image: 59
+  - Destination: Kidsgrove
+    Date: 2003-09-24 14:22:30
+    Road: A34
+    Image: 60
+  - Destination: Stoke On Trent
+    Date: 2003-09-24 15:45:02
+    Road: A34
+    Image: 61
+  - Destination: Stone
+    Date: 2003-09-24 15:45:07
+    Road: A34
+  - Destination: Yarlett
+    Date: 2003-09-24 15:45:10
+    Road: A34
+    Image: 63
+  - Destination: Stafford
+    Date: 2003-09-24 15:45:16
+    Road: A34
+  - Destination: Dunston
+    Date: 2003-09-24 15:45:24
+    Road: A449
+  - Destination: Codsall
+    Date: 2003-09-24 15:47:35
+    Road: A449
+  - Destination: Birmingham
+    Date: 2003-09-24 18:12:27
+    Road: Local
+  - Destination: Birmingham
+    Date: 2003-09-25 04:04:08
+    Road: Local
+    Image: 68
+  - Destination: Bromsgrove
+    Date: 2003-09-25 05:12:00
+    Road: A38
+    Image: 69
+  - Destination: Droitwich
+    Date: 2003-09-25 05:45:59
+    Road: A38
+    Image: 70
+  - Destination: Worcester
+    Date: 2003-09-25 06:23:25
+    Road: A38
+    Image: 71
+  - Destination: Tewkesbury
+    Date: 2003-09-25 07:27:36
+    Road: A38
+    Image: 72
+  - Destination: Gloucester
+    Date: 2003-09-25 08:40:43
+    Road: A38
+    Image: 73
+  - Destination: Cambridge
+    Date: 2003-09-25 10:12:17
+    Road: A38
+    Image: 74
+  - Destination: Newport
+    Date: 2003-09-25 10:38:47
+    Road: A38
+    Image: 75
+  - Destination: Stone
+    Date: 2003-09-25 10:44:03
+    Road: A38
+  - Destination: Almondsbury
+    Date: 2003-09-25 11:20:07
+    Road: A38
+    Image: 77
+  - Destination: Patchway
+    Date: 2003-09-25 11:21:10
+    Road: A38
+    Image: 78
+  - Destination: Filton
+    Date: 2003-09-25 11:32:13
+    Road: A38
+  - Destination: Bristol
+    Date: 2003-09-25 11:57:33
+    Road: A38
+  - Destination: Cowslip Green
+    Date: 2003-09-25 13:19:13
+    Road: A38
+  - Destination: Lower Wear
+    Date: 2003-09-25 13:30:48
+    Road: A38
+  - Destination: Tarnock
+    Date: 2003-09-25 13:40:49
+    Road: A38
+  - Destination: Huntspill
+    Date: 2003-09-25 14:04:29
+    Road: A38
+    Image: 84
+  - Destination: Bridgewater
+    Date: 2003-09-25 14:23:31
+    Road: A38
+    Image: 85
+  - Destination: Taunton
+    Date: 2003-09-25 15:15:17
+    Road: A38
+    Image: 86
+  - Destination: Corfe
+    Date: 2003-09-25 15:54:32
+    Road: B3170
+  - Destination: Newcott
+    Date: 2003-09-25 16:54:25
+    Road: Old A30
+    Image: 88
+  - Destination: Monkton
+    Date: 2003-09-25 17:16:09
+    Road: Old A30
+    Image: 89
+  - Destination: Honiton
+    Date: 2003-09-25 17:41:02
+    Road: Old A30
+  - Destination: Rockbere
+    Date: 2003-09-25 17:42:20
+    Road: Old A30
+  - Destination: Wimple
+    Date: 2003-09-25 17:42:37
+    Road: Old A30
+  - Destination: Exeter Airport
+    Date: 2003-09-25 17:42:47
+    Road: Old A30
+  - Destination: Newton Poppleford
+    Date: 2003-09-25 17:50:49
+    Road: B Road
+  - Destination: Newton Poppleford
+    Date: 2003-09-26 08:19:39
+    Road: B Road
+    Image: 95
+  - Destination: Exeter
+    Date: 2003-09-26 08:19:55
+    Road: A3052
+    Image: 96
+  - Destination: Okehampton
+    Date: 2003-09-26 09:16:33
+    Road: A30
+  - Destination: Launceston
+    Date: 2003-09-26 11:36:56
+    Road: A30
+    Image: 98
+  - Destination: Bodmin
+    Date: 2003-09-26 12:58:41
+    Road: A30
+    Image: 99
+  - Destination: Redruth
+    Date: 2003-09-26 16:05:06
+    Road: A30
+    Image: 100
+  - Destination: Cambourne
+    Date: 2003-09-26 16:10:26
+    Road: A30
+    Image: 101
+  - Destination: Penzance
+    Date: 2003-09-26 17:19:26
+    Road: A30
+    Image: 102
+  - Destination: Drift
+    Date: 2003-09-26 17:31:23
+    Road: A30
+    Image: 103
+  - Destination: Crows-An-Wra
+    Date: 2003-09-26 17:44:41
+    Road: A30
+    Image: 104
+  - Destination: Sennen
+    Date: 2003-09-26 17:51:06
+    Road: A30
+    Image: 106
+  - Destination: Lands End
+    Date: 2003-09-26 17:58:57
+    Road: A30
+    Image: 107
+  
+`
+
+export default timeline
